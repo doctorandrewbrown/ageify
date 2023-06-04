8bb9de7265cf3b0ab94fb50b48a857a222fc56ca
Andy Brown <dr.andrew.david.brown@gmail.com>
2023-06-04 22:49:27 +0100

    implement  fetch to api with user input fom form

---
+++ README.md
@@ -0,0 +1,2 @@
+fetch api ref
+https://www.digitalocean.com/community/tutorials/how-to-use-the-javascript-fetch-api-to-get-data
--- assets/scripts.js
+++ assets/scripts.js
@@ -4,12 +4,27 @@ document.addEventListener("DOMContentLoaded", ()=>{
     // grab form element
     const form = document.querySelector("form")
 
-    // listen for submit event on form and prevent this allowing user input to be grabbed
+    // explicitly listen for submit event and prevent this to allow form input data to be grabbed
     form.addEventListener("submit", (event)=> {
         event.preventDefault()
+
+        // get user input from form
         const name = document.querySelector("#name").value
-        
-        // log to console to test functionality
-        console.log(name)
+
+        // insert user input into url call
+        const url = `https://api.agify.io?name=${name}`
+
+        // make api call see 
+        //https://www.digitalocean.com/community/tutorials/how-to-use-the-javascript-fetch-api-to-get-data
+
+        fetch(url)
+        .then((response) => {
+            return response.json();
+          })
+          .then((data) => {
+            
+            // log response object to console to test functionality
+            console.log(data)
+          })
     })
 })
