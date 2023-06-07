d26e76623dde9388140dcaf8daec90011791a588
Andy Brown <dr.andrew.david.brown@gmail.com>
2023-06-07 12:21:36 +0100

    moves api func call definition to correct scope

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,30 +1,37 @@
+
 // wait for DOM to load before getting any element
-document.addEventListener("DOMContentLoaded", ()=>{
+document.addEventListener("DOMContentLoaded", () => {
 
     // grab form element
     const form = document.querySelector("form")
 
     // explicitly listen for submit event and prevent this to allow form input data to be grabbed
-    form.addEventListener("submit", (event)=> {
+    form.addEventListener("submit", (event) => {
         event.preventDefault()
 
         // get user input from form
-        const name = document.querySelector("#name").value
+        const inputName = document.querySelector("#inputName").value
 
-        // insert user input into url call
-        const url = `https://api.agify.io?name=${name}`
-
-        // make api call see 
-        //https://www.digitalocean.com/community/tutorials/how-to-use-the-javascript-fetch-api-to-get-data
-
-        fetch(url)
-        .then((response) => {
-            return response.json();
-          })
-          .then((data) => {
+        // define function to make api call
+        function agify() {
             
-            // log response object to console to test functionality
-            console.log(data)
-          })
+            // insert user input into url call
+            const url = `https://api.agify.io?name=${inputName}`
+        
+            // make api call see 
+            // https://www.digitalocean.com/community/tutorials/how-to-use-the-javascript-fetch-api-to-get-data
+        
+            fetch(url)
+                .then((response) => {
+                    return response.json();
+                })
+                .then((data) => {
+        
+                    // log response object to console to test functionality
+                    console.log(data)
+                })
+        }
+        agify()
     })
 })
+
