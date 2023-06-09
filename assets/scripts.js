6f5c113e7f4b4ed122b2d790c239a9ada1e43ae7
Andy Brown <dr.andrew.david.brown@gmail.com>
2023-06-09 16:44:27 +0100

    moves variable declarations to global scope

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,6 +1,8 @@
+let inputName
+let inputCountry
+let apiData
 // wait for DOM to load before getting any element
 document.addEventListener("DOMContentLoaded", () => {
-
     // grab form element
     const form = document.querySelector("form")
 
@@ -8,46 +10,20 @@ document.addEventListener("DOMContentLoaded", () => {
     form.addEventListener("submit", (event) => {
         event.preventDefault()
 
-        // get user input from form
-        const inputName = document.querySelector("#inputName").value
-        const inputCountry = document.querySelector("#inputCountry").value
+        // get user input from form once DOM loaded
+         inputName = document.querySelector("#firstName").value
+         inputCountry = document.querySelector("#country").value
 
         // declare empty object for data returned by api
-        let apiData
+        //let apiData
 
-        // define function to call ageify api
-        function agify() {
-
-            // insert user input into url call
-            const url = `https://api.agify.io?name=${inputName}&country_id=${inputCountry}`
-
-            // make api call see 
-            // https://www.digitalocean.com/community/tutorials/how-to-use-the-javascript-fetch-api-to-get-data
-
-            fetch(url)
-                .then((response) => {
-                    return response.json();
-                })
-                .then((data) => {
-                    let ageData = data
-
-                    // put api age data response into apiData object using spread operator
-
-                    apiData = {
-                        ...apiData,
-                        ...ageData
-                    }
-
-                    // log response object to console to test functionality
-                    console.log(apiData)
-                })
-        }
         // define function to call genderize api
 
         function genderize() {
 
             // insert user input into url call
             const url = `https://api.genderize.io?name=${inputName}&country_id=${inputCountry}`
+            console.log(inputName)
 
             // make api call see 
             // https://www.digitalocean.com/community/tutorials/how-to-use-the-javascript-fetch-api-to-get-data
@@ -69,15 +45,20 @@ document.addEventListener("DOMContentLoaded", () => {
                     // log response object to console to test functionality
 
                     console.log("genderize")
+                    console.log(apiData)
+
                 })
                 // results data only logged after api data received
                 .then(() => {
+                    // log combined data object
+
                     console.log(apiData)
+
                     // create list element
                     const record = document.createElement("li")
 
                     // insert api data in list for display
-                    record.innerHTML = `name: ${apiData.name}, age: ${apiData.age}, gender: ${apiData.gender}.`
+                    record.innerHTML = `name: ${apiData.name}, age: ${apiData.age}, gender: ${apiData.gender}, country: ${apiData.country_id}.`
 
                     // append api data to list in user interface
                     const list = document.querySelector("#records")
@@ -89,3 +70,34 @@ document.addEventListener("DOMContentLoaded", () => {
         genderize()
     })
 })
+
+// define function to call ageify apifunction test_warning(){
+function agify() {
+
+    // insert user input into url call
+    const url = `https://api.agify.io?name=${inputName}&country_id=${inputCountry}`
+    console.log(inputName)
+    // make api call see 
+    // https://www.digitalocean.com/community/tutorials/how-to-use-the-javascript-fetch-api-to-get-data
+
+    fetch(url)
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            let ageData = data
+
+            // put api age data response into apiData object using spread operator
+
+            apiData = {
+                ...apiData,
+                ...ageData
+            }
+
+            // log response object to console to test functionality
+            console.log("ageify")
+
+            console.log(apiData)
+        })
+}
+
