bbb08d1a58e2332b46a47d33bd66b1b3524cd4c8
Andy Brown <dr.andrew.david.brown@gmail.com>
2023-06-10 17:19:30 +0100

    removed genderize api functionality

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,6 +1,11 @@
+// declare variables globally
+
 let inputName
 let inputCountry
+
+// empty object to receive api data
 let apiData
+
 // wait for DOM to load before getting any element
 document.addEventListener("DOMContentLoaded", () => {
     // grab form element
@@ -11,24 +16,21 @@ document.addEventListener("DOMContentLoaded", () => {
         event.preventDefault()
 
         // get user input from form once DOM loaded
-         inputName = document.querySelector("#firstName").value
-         inputCountry = document.querySelector("#country").value
+        inputName = document.querySelector("#firstName").value
+        inputCountry = document.querySelector("#country").value
 
-        // declare empty object for data returned by api
-        //let apiData
-
-        // call functions
+        // call function to call api
         agify()
-        genderize()
     })
 })
 
-// define function to call ageify apifunction test_warning(){
+// define function to call ageify api
 function agify() {
-
+    console.log("ageify")
     // insert user input into url call
     const url = `https://api.agify.io?name=${inputName}&country_id=${inputCountry}`
     console.log(inputName)
+
     // make api call see 
     // https://www.digitalocean.com/community/tutorials/how-to-use-the-javascript-fetch-api-to-get-data
 
@@ -45,59 +47,55 @@ function agify() {
                 ...apiData,
                 ...ageData
             }
+            console.log(apiData)
+            display()
+        })
+        //.then(display())
+}
+// define function to call genderize api
+
+/*function genderize() {
+
+    // insert user input into url call
+    const url = `https://api.genderize.io?name=${inputName}&country_id=${inputCountry}`
+    console.log(inputName)
+
+    // make api call see 
+    // https://www.digitalocean.com/community/tutorials/how-to-use-the-javascript-fetch-api-to-get-data
+
+    fetch(url)
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            let genderData = data
+
+            // put api gender data response into apiData object using spread operator
+
+            apiData = {
+                ...apiData,
+                ...genderData
+            }
 
             // log response object to console to test functionality
-            console.log("ageify")
 
+            console.log("genderize")
             console.log(apiData)
+
         })
+}*/
+function display(){
+    console.log("display")
+    // log combined data object
+    console.log(apiData)
+
+    // create list element
+    const record = document.createElement("li")
+
+    // insert api data in list for display
+    record.innerHTML = `name: ${apiData.name}, age: ${apiData.age}, count: ${apiData.count}, country: ${apiData.country_id}.`
+
+    // append api data to list in user interface
+    const list = document.querySelector("#records")
+    list.append(record)
 }
-        // define function to call genderize api
-
-        function genderize() {
-
-            // insert user input into url call
-            const url = `https://api.genderize.io?name=${inputName}&country_id=${inputCountry}`
-            console.log(inputName)
-
-            // make api call see 
-            // https://www.digitalocean.com/community/tutorials/how-to-use-the-javascript-fetch-api-to-get-data
-
-            fetch(url)
-                .then((response) => {
-                    return response.json();
-                })
-                .then((data) => {
-                    let genderData = data
-
-                    // put api gender data response into apiData object using spread operator
-
-                    apiData = {
-                        ...apiData,
-                        ...genderData
-                    }
-
-                    // log response object to console to test functionality
-
-                    console.log("genderize")
-                    console.log(apiData)
-
-                })
-                // results data only logged after api data received
-                .then(() => {
-                    // log combined data object
-
-                    console.log(apiData)
-
-                    // create list element
-                    const record = document.createElement("li")
-
-                    // insert api data in list for display
-                    record.innerHTML = `name: ${apiData.name}, age: ${apiData.age}, gender: ${apiData.gender}, country: ${apiData.country_id}.`
-
-                    // append api data to list in user interface
-                    const list = document.querySelector("#records")
-                    list.append(record)
-                })
-        }
-
