ec7492f9291946d8b0eaf697ea6e5465a4847510
Andy Brown <dr.andrew.david.brown@gmail.com>
2023-06-18 16:34:54 +0100

    Adds new results object. Renames api count data

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,26 +1,26 @@
 // declare variables for user input
-let inputName
-let inputCountry
+let inputName;
+let inputCountry;
 
-// declare empty object to receive api data
-let apiData
+// declare new empty object to receive api data
+let apiData = new Object;
 
 // wait for DOM to load before getting any element
 document.addEventListener("DOMContentLoaded", () => {
 
     // grab form element
-    const form = document.querySelector("form")
+    const form = document.querySelector("form");
 
     //  listen for submit event and prevent this to allow form input data to be grabbed
     form.addEventListener("submit", (event) => {
-        event.preventDefault()
+        event.preventDefault();
 
         // get user input from form once DOM loaded and submit button clicked
-        inputName = document.querySelector("#firstName").value
-        inputCountry = document.querySelector("#country").value
+        inputName = document.querySelector("#firstName").value;
+        inputCountry = document.querySelector("#country").value;
 
         /* start main program */
-        getAgeData(getGenderData)
+        getAgeData(getGenderData);
     })
 })
 
@@ -28,7 +28,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 // define function to call agify api
 function getAgeData(cb) {
-    console.log("getAgeData")
+    console.log("getAgeData");
     // declare request object
     const xhttp = new XMLHttpRequest();
 
@@ -40,10 +40,13 @@ function getAgeData(cb) {
         // check if api data received before updating results
         // this. refers to xhttp object
         if (this.readyState == 4 && this.status == 200) {
-            console.log(xhttp.status)
             // put api response into json format once data received from api
-            let ageData = JSON.parse(this.responseText)
-
+            let ageData = JSON.parse(this.responseText);
+            console.log(ageData)
+            // rename count data to age_count to prevent overwriting when data from apis is merged
+            const ageCount = ageData.count
+            apiData.age_count = ageCount
+            console.log(apiData)
             // put age data in results object
             apiData = {
                 ...apiData,
@@ -51,7 +54,7 @@ function getAgeData(cb) {
             }
 
             // call callback function getGenderData and pass display function
-            cb(display)
+            cb(display);
         }
     };
 }
@@ -71,18 +74,22 @@ function getGenderData(cb) {
         // check if api data received before updating results
 
         if (this.readyState == 4 && this.status == 200) {
-            console.log(xhttp.status)
+        
             // put api response into json format
-            let genderData = JSON.parse(this.responseText)
-            console.log(genderData)
+            let genderData = JSON.parse(this.responseText);
+            console.log(genderData);
+            // rename count data to gender_count to prevent overwriting when data from apis is merged
+            const genderCount = genderData.count
+            apiData.gender_count = genderCount
+            console.log(apiData)
             // put gender data in results object
             apiData = {
                 ...apiData,
                 ...genderData
             }
 
-            // call callback function display() to display results on user interface
-            cb()
+            // callback function display() to display results on user interface
+            cb();
         }
     }
 }
@@ -90,21 +97,21 @@ function getGenderData(cb) {
 // define function to display results on user interface
 
 function display() {
-    console.log(apiData)
+    console.log(apiData);
 
     // create object for getting english names from country codes. See MDN
     const regionNamesInEnglish = new Intl.DisplayNames(['en'], {
         type: 'region'
     });
 
-    const countryName = regionNamesInEnglish.of(apiData.country_id)
+    const countryName = regionNamesInEnglish.of(apiData.country_id);
 
     // create list element
-    let record = document.createElement("li")
-    
+    let record = document.createElement("li");
+
     // add bootstrap class to li elements for spacing results records in user interface
     // https://stackoverflow.com/questions/74132721/bootstrap-5-center-text-in-li
-    record.classList.add("mt-3", "d-flex", "align-items-center")
+    record.classList.add("mt-3", "d-flex", "align-items-center");
 
     // check data returned by api for missing gender and age data and provide "no data" message to user
     if (apiData.age === null) {
@@ -114,23 +121,23 @@ function display() {
     if (apiData.gender === null) {
         apiData.gender = "no data"
     }
-    
+
     // if no age or gender data returned from api show error message to user
     if (apiData.age === "no data" && apiData.gender === "no data") {
-    
+
         record.innerHTML = " There is no data available for " + `${inputName}` + " in " + `${countryName}`
         record.style.color = "red"
 
-    } 
+    }
 
     // show result data to user
     else {
         // insert api data in list for display
-        record.innerHTML =  `name: ${apiData.name}; country: ${countryName}; average age: ${apiData.age}; gender: ${apiData.gender}; probability ${apiData.probability}; gender count: ${apiData.count}.`
+        record.innerHTML = `name: ${apiData.name}; country: ${countryName}; average age: ${apiData.age}; gender: ${apiData.gender}; probability ${apiData.probability}; gender count: ${apiData.count}.`
 
     }
 
     // append api data to list in user interface
-    const list = document.querySelector("#records")
-    list.append(record)
+    const list = document.querySelector("#records");
+    list.append(record);
 }
